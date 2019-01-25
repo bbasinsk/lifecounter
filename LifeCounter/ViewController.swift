6cb56098f28eabd8c6f3db634a2a35c00c55b0ac
Ben Basinski <benbasinski97@gmail.com>
2019-01-24 18:49:57 -0800

    Initial Commit

---
+++ LifeCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifeCounter
+//
+//  Created by Ben Basinski on 1/24/19.
+//  Copyright © 2019 Ben Basinski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ LifeCounterTests/LifeCounterTests.swift
@@ -0,0 +1,34 @@
+//
+//  LifeCounterTests.swift
+//  LifeCounterTests
+//
+//  Created by Ben Basinski on 1/24/19.
+//  Copyright © 2019 Ben Basinski. All rights reserved.
+//
+
+import XCTest
+@testable import LifeCounter
+
+class LifeCounterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
